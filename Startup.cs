7e1b1c1d125df2ce7eadf1d8ebefc8c827326aea
aziza <47671886+azizaElsayed@users.noreply.github.com>
2019-08-16 09:16:08 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+﻿using JobOffers.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobOffers.Startup))]
+namespace JobOffers
+{
+    public partial class Startup
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+           // createDefaultRolesAndUser();
+        }
+
+
+        
+    }
+}
